94e0bf8101dff2d7a4c1242c6932894d8d31f2a2
capimaker <capimaker@gmail.com>
2025-07-17 17:47:29 +0200

    index preparado

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require('express');
+const { dbConnection } = require('./config/config');
+//const { handleTypeError } = require('./middlewares/errors');
+const app = express();
+const PORT = process.env.PORT;
+//const swaggerUI = require('swagger-ui-express');
+//const docs = require('./docs/index');
+
+dbConnection();
+
+// MIDDLEWARE
+app.use(express.json());
+
+// ENDPOINTS
+//app.use('/users', require('./routes/users'));
+//app.use('/posts', require('./routes/posts'));
+//app.use('/comments', require('./routes/comments'));
+//app.use('/uploads', express.static('uploads'));
+
+//app.use(handleTypeError);
+
+//app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(docs));
+
+// SERVER
+app.listen(PORT, () => {
+  console.log(`Server started on port http://localhost:${PORT}`);
+});
