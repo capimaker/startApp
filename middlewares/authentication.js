832443274074e1640cece11348c47e8c0f42493c
alejandrogoscu <alejandrogoscu@gmail.com>
2025-07-17 19:38:08 +0200

    middleware auth OK

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,76 +1,49 @@
-const Comment = require("../models/Comment.js");
-const Post = require("../models/post.js");
-const User = require("../models/user.js");
-require("dotenv").config();
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
+const Startup = require('../models/Startup');
+const Mentor = require('../models/Mentor');
+const Admin = require('../models/Admin');
 const JWT_SIGNATURE = process.env.JWT_SECRET;
 
 const authentication = async (req, res, next) => {
   try {
-    const authHeader = req.headers.authorization;
+    const token = req.headers.authorization?.replace('Bearer ', '');
 
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-      return res
-        .status(401)
-        .send({ message: "Token no proporcionado o formato incorrecto" });
+    if (!token) {
+      return res.status(401).send({ message: 'Token no proporcionado o formato incorrecto' });
     }
 
-    const token = authHeader.replace("Bearer ", "");
-
     const payload = jwt.verify(token, JWT_SIGNATURE);
-    const user = await User.findOne({
-      _id: payload._id,
-      "tokens.token": token,
-    });
+    const { _id, role } = payload;
+
+    let user;
+
+    switch (role) {
+      case 'startup':
+        user = await Startup.findOne({ _id, 'tokens.token': token });
+        break;
+      case 'mentor':
+        user = await Mentor.findOne({ _id, 'tokens.token': token });
+        break;
+      case 'admin':
+        user = await Admin.findOne({ _id, 'tokens.token': token });
+        break;
+      default:
+        return res.status(401).send({ message: 'Rol de usuario no válido' });
+    }
 
     if (!user) {
-      return res
-        .status(401)
-        .send({ message: "Token no válido o usuario no encontrado" });
+      return res.status(401).send({ message: 'Token no válido o usuario no encontrado' });
     }
 
     req.user = user;
     req.token = token;
+    req.role = role;
 
     next();
   } catch (error) {
-    console.error("Error en autenticación:", error.message);
-    return res
-      .status(401)
-      .send({ error: error.message || error, message: "Token inválido" });
+    console.error('Error en autenticación:', error.message);
+    res.status(401).send({ message: 'Token inválido', error: error.message });
   }
 };
 
-const isAuthorComment = async (req, res, next) => {
-  try {
-    const comment = await Comment.findById(req.params._id);
-    if (comment.user.toString() !== req.user._id.toString()) {
-      return res.status(403).send({ message: "Este comentario no es tuyo" });
-    }
-    next();
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send({
-      error,
-      message: "Ha habido un problema al comprobar la autoría del comentario",
-    });
-  }
-};
-
-const isAuthorPost = async (req, res, next) => {
-  try {
-    const post = await Post.findById(req.params._id);
-    if (post.user.toString() !== req.user._id.toString()) {
-      return res.stauts(403).send({ message: "Esta publicación no es tuya" });
-    }
-    next();
-  } catch (error) {
-    console.error(error);
-    return res.status(500).send({
-      error,
-      message: "Ha habido un problema al verificar que este post es tuyo",
-    });
-  }
-};
-
-module.exports = { authentication, isAuthorComment, isAuthorPost };
+module.exports = { authentication };
